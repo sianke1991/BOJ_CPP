c0cfc40d1bd98a788372958681d6d33a2a67fe9d
sianke1991 <90839850+sianke1991@users.noreply.github.com>
2022-10-02 19:08:07 +0900

    Update stack01.cpp
    추가 설명을 주석으로 작성함

--- 20221002_dataStructure00/stack01.cpp
+++ 20221002_dataStructure00/stack01.cpp
@@ -20,3 +20,20 @@ int main() {
 
     return 0;
 }
+/*
+stack, FILO 자료형 (= 제일 먼저 들어간 원소가 제일 나중에 나온다.)
+추가 및 제거시 O(1)의 시간이 필요하다.
+(마지막에 추가되어서) 맨 먼저 나오게 되는 원소만 조회할 수 있다.
+
+C++에서 제공하는 스택에서 주로 사용하는 메서드
+push(elem)
+pop()
+size() - int 반환
+empty() - 0 또는 1 반환
+top() - elem 자료형 반환
+
+Restricted Structures
+  stack
+  queue
+  deck
+*/
